d5285bc0df1b9aa3d79e9f206b783a166151972c
Ujjwal3009 <50870185+Ujjwal3009@users.noreply.github.com>
2020-07-08 18:27:23 +0530

    Delete index.php

--- admin/index.php
+++
@@ -1,8 +0,0 @@
-<?php include "includes/header.php";?>
-<?php include "includes/navigation.php"; ?>
-<?php include "main.php"; ?>
-<?php include "includes/footer.php";?>
-<?php  include "includes/script.php";?>
-<!--===========================================================================================================-->
-</body>
-</html>
